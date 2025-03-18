d73401bffaa1928a56ad164078c8d83ef93fe1ed
agal07 <146463603+agal07@users.noreply.github.com>
2025-03-18 21:44:19 +0530

    Create expense_tracker.py

---
+++ expense_tracker.py
@@ -0,0 +1,7 @@
+cd path/to/your/project
+git init
+git add .
+git commit -m "Initial commit - Upload expense tracker project"
+git branch -M main
+git remote add origin https://github.com/agal07/Personal-Expense-Tracker.git
+git push -u origin main
